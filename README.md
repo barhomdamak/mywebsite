9ac3cb27f9924e8a86385757104c913f4d8eac92
barhomdamak <brobarhomdamak@gmail.com>
2019-03-28 19:46:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-avlcub
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-avlcub)
---
+++ index.ts
@@ -0,0 +1,8 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('name');
+const Bio:  HTMLElement = document.getElementById('bio');
+appDiv.innerHTML = `<h1>Ibrahim dammak</h1>`;
+Bio.innerHTML = '<h2>this is my bio</h2>';
